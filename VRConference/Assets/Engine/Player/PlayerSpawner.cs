db993eac4bc8a637c689629c3d5217f6aff3b562
Stroby241 <46872913+Stroby241@users.noreply.github.com>
2021-12-26 13:41:01 +0100

    VR intigrated

--- VRConference/Assets/Engine/Player/PlayerSpawner.cs
+++ VRConference/Assets/Engine/Player/PlayerSpawner.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.XR.Management;
 using Utility;
 
 namespace Engine.Player
@@ -22,8 +22,46 @@ namespace Engine.Player
             playerStart.Register(() =>
             {
                 playerFaetureState.value = (int) FeatureState.starting;
-                player = Instantiate(isVr.value ? playerVr : playerFirstPerson, world.transform);
-                playerFaetureState.value = (int) FeatureState.online;
+
+                if (!isVr.value)
+                {
+                    player = Instantiate(playerFirstPerson, world.transform);
+                    playerFaetureState.value = (int) FeatureState.online;
+                    return;
+                    
+                }
+                else
+                {
+                    Debug.Log("Initializing XR...");
+                    XRGeneralSettings.Instance.Manager.InitializeLoader();
+
+
+                    if (XRGeneralSettings.Instance.Manager.activeLoader == null)
+                    {
+                        Debug.LogError("Initializing XR Failed. Check Editor or Player log for details.");
+                        playerFaetureState.value = (int) FeatureState.failed;
+                    }
+                    else
+                    {
+                        Debug.Log("Starting XR...");
+                        XRGeneralSettings.Instance.Manager.activeLoader.Start();
+                        XRGeneralSettings.Instance.Manager.StartSubsystems();
+            
+                        XRGeneralSettings.Instance.Manager.InitializeLoader();
+            
+                        //yield return new WaitForSecondsRealtime(5);
+
+
+                        var playertest = Instantiate(VR_Player, new Vector3(0, 0, 0), Quaternion.identity);
+                        Instantiate(VR_Teleporting, new Vector3(0, 0, 0), Quaternion.identity);
+                        Instantiate(VR_TeleportPoint, new Vector3(0, 0, 0), Quaternion.identity);
+                        Instantiate(VR_TeleportPoint, new Vector3(0, 0, 0), Quaternion.identity);
+                        var test = Instantiate(VR_Menu, new Vector3(0.02485144f, 1.229f, 5.52f), Quaternion.identity);
+                        test.transform.localScale = new Vector3(0.005f, 0.005f, 0.005f);
+                        test.transform.GetChild(0).GetComponent<Canvas>().worldCamera = playertest.transform.GetChild(0).transform.GetChild(2).GetChild(4).GetComponent<Camera>();
+                        playerFaetureState.value = (int) FeatureState.online;
+                    }
+                }
             });
 
             playerStop.Register(() =>
@@ -35,13 +73,24 @@ namespace Engine.Player
         }
 
         [SerializeField] private GameObject playerFirstPerson;
-        [SerializeField] private GameObject playerVr;
         [SerializeField] private PublicBool isVr;
         [SerializeField] private PublicEvent playerStart;
         [SerializeField] private PublicEvent playerStop;
         [SerializeField] private PublicInt playerFaetureState;
         [SerializeField] private GameObject world;
+        
+        public GameObject VR_Player;
+        public GameObject VR_Teleporting;
+        public GameObject VR_TeleportPoint;
+
+        public GameObject VR_Menu;
 
         private GameObject player;
+        
+        private void OnDestroy()
+        {
+            XRGeneralSettings.Instance.Manager.DeinitializeLoader();
+            XRGeneralSettings.Instance.Manager.StartSubsystems();
+        }
     }
 }
