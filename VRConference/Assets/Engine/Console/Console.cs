4999d716d9d843b8ca499784979863093cacc152
Stroby241 <maarten.behn@gmail.com>
2022-01-14 18:43:34 +0100

    scene save

---
+++ VRConference/Assets/Engine/Console/Console.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utility.Console
+{
+    public class Console : MonoBehaviour
+    {
+        [SerializeField] private GameObject messagePreFab;
+        [SerializeField] private Transform content;
+
+        private int id;
+        void OnEnable () {
+            foreach (ConsoleHandler.ConsoleMessage message in ConsoleHandler.instance.messages)
+            {
+                Spawn(message.message, message.startTime);
+            }
+
+            id = ConsoleHandler.instance.newLogMessage.Register(message =>
+            {
+                Spawn(message, Time.time);
+            });
+        }
+
+        void OnDisable () {
+            ConsoleHandler.instance.newLogMessage.Unregister(id);
+        }
+
+        void Spawn (String logString, float startTime)
+        {
+            ConsoleMessage message = Instantiate(messagePreFab, content).GetComponent<ConsoleMessage>();
+            message.text.text = logString;
+            message.startTime = startTime;
+            message.liveTime = ConsoleHandler.instance.liveTime;
+        }
+    }
+}
