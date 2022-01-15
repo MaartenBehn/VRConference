feacb349d1b3c9593d1ed0a0a3ec3840c3b72753
Maarten Behn <maarten.behn@gmail.com>
2022-01-15 01:32:44 +0100

    ui

---
+++ VRConference/Assets/Menus/ThemeElement.cs
@@ -0,0 +1,61 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Menus{
+	
+	[ExecuteInEditMode()]
+	[System.Serializable]
+	public class ThemeElement : MonoBehaviour {
+		
+		public ThemeSetting themeSettings;
+		private Image image;
+		private TMP_Text text;
+		public bool imageHighlight = false;
+		public bool imageBase = false;
+		public bool isText = false;
+		
+		public virtual void Awake()
+		{
+			
+			OnSkinUI();
+		}
+
+		public virtual void Update()
+		{
+			OnSkinUI();
+		}
+
+		void OnSkinUI(){
+			if (themeSettings == null)
+			{
+				Debug.Log("Theme missing");
+				return;
+			}
+
+			if(imageHighlight){
+				if (image == null)
+				{
+					image = GetComponent<Image>();
+				}
+				image.color = themeSettings.highlightColor;
+			}
+			
+			if(imageBase){
+				if (image == null)
+				{
+					image = GetComponent<Image>();
+				}
+				image.color = themeSettings.baseColor;
+			}
+			
+			if (isText) {
+				if (text == null)
+				{
+					text = GetComponent<TMP_Text>();
+				}
+				text.color = themeSettings.textColor;
+			}
+		}
+	}
+}
---
+++ VRConference/Assets/Menus/ThemeSetting.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Menus{
+	[CreateAssetMenu(menuName = "ThemeSettings")]
+	[System.Serializable]
+	public class ThemeSetting : ScriptableObject {
+		
+		public Color highlightColor;
+		public Color baseColor;
+		public Color32 textColor;
+	}
+}
